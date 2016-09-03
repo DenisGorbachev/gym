b6703858b210351839b952bf960cbfe99c7693f7
Denis Gorbachev <denis.d.gorbachev@gmail.com>
2016-09-03 17:20:52 +0300

    Solve manually (average reward > 0.78)

--- exercise.py
+++ exercise.py
@@ -1,36 +1,46 @@
 import gym
 import registry
 
-env = gym.make('FrozenLakeDeterministic-v0')
-observation = env.reset()
 
-# 0 - Left
-# 1 - Down
-# 2 - Right
-# 3 - Up
+action_names = ["Left", "Down", "Right", "Up"]
+
+# env = gym.make('FrozenLakeDeterministic-v0')
+# actions = [
+#     1, 2, 1, 0,
+#     1, 7, 1, 7,
+#     2, 1, 1, 7,
+#     7, 2, 2, 9,
+# ]
+
+env = gym.make('FrozenLake-v0')
 actions = [
-    1, 2, 1, 0,
-    1, 7, 1, 7,
-    2, 1, 1, 7,
-    7, 2, 2, 9,
+    0, 3, 3, 3,
+    0, 7, 0, 7,
+    3, 1, 0, 7,
+    7, 2, 1, 9,
 ]
 
-for t in range(10):
-    print("========")
-    dir(env.action_space)
-    env.render()
-    print(observation)
-    action = actions[observation]  # actions.pop()
-    print(action)
-    observation, reward, done, info = env.step(action)
-    print(info)
-    if done:
-        print("########")
-        env.render()
-        print(observation)
-        print("Episode finished after {} timesteps".format(t + 1))
-        if reward:
-            print("Win!")
-        else:
-            print("Loss!")
-        break
+total_reward = 0
+max_episodes = 500
+
+for episode in range(max_episodes):
+    print("==== Begin Episode {} ====".format(episode))
+    observation = env.reset()
+    done = False
+    steps = 0
+    while not done:
+        # env.render()
+        action = actions[observation]  # actions.pop()
+        # print("-> {}".format(action_names[action]))
+        observation, reward, done, info = env.step(action)
+        steps += 1
+    # env.render()
+    print("#### End Episode {} after {} steps ####".format(episode, steps))
+    total_reward += reward
+    if reward:
+        print("Win!")
+    else:
+        print("Loss!")
+        # break
+
+print("Total reward: {} / {} @ {}".format(total_reward, max_episodes, total_reward / max_episodes))
